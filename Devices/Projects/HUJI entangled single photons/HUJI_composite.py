225b50f49a8f4e963219dceec2b8ba39ca48d4aa
slookma <slookma@biu.ac.il>
2024-03-10 00:50:53 +0200

    Create HUJI_composite.py

---
+++ Devices/Projects/HUJI entangled single photons/HUJI_composite.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 9 19:04:20 2024
+
+@author: matan slook
+"""
+
+import nazca as nd
+
+# this part is for the composite part
+taper = 1
+w1 = 0.66
+w2 = 0.89
+L1 = 24.39
+L2 = 21.83
+
+WG_width = 0.7
+Gap = 1-WG_width
+WG_radius = 100
+bus1 = 10
+bus2 = 100
+Lc = [8.9,9,9.06,9.1,9.2,9.3] # Lc is 31.6 um - might be 20 um
+GC_gap = 127
+ld = 250 #55  layer numberr
+
+
+
+def coupler(Width,gap,Length,Radius,positionX,positionY,busLength1,busLength2):
+    """
+    Directional Coupler.
+    width           : width of the waveguides
+    gap             : gap between the center of the waveguides
+    Length          : length the waveguide travel together
+    Radius         : radius of the circle of the transition regions
+    position        : coupler position (feed point)
+    busLength1,2    : length of the bus before/after the coupler
+    """
+    with nd.Cell(name='Directional Coupler') as dc:
+        elm1 = nd.strt(length=busLength1,width=Width,layer=ld).put(positionX,positionY)
+        nd.bend(angle=90,radius=Radius,width=Width,layer=ld).put()
+        nd.strt(length=75,width=Width,layer=ld).put()
+        nd.bend(angle=-90,radius=Radius,width=Width,layer=ld).put()
+        nd.strt(length=Length,width=Width,layer=ld).put()
+        nd.bend(angle=-90,radius=Radius,width=Width,layer=ld).put()
+        elm2 = nd.strt(length=busLength2,width=Width,layer=ld).put()    
+                
+        positionY += 4*float(Radius)+float(Width+gap)+150
+        elm3 = nd.strt(length=busLength1,width=Width,layer=ld).put(positionX,positionY)
+        nd.bend(angle=-90,radius=Radius,width=Width,layer=ld).put()
+        nd.strt(length=75,width=Width,layer=ld).put()
+        nd.bend(angle=90,radius=Radius,width=Width,layer=ld).put()
+        nd.strt(length=Length,width=Width,layer=ld).put()
+        nd.bend(angle=90,radius=Radius,width=Width,layer=ld).put()
+        elm4 = nd.strt(length=busLength2,width=Width,layer=ld).put()
+        
+        nd.Pin('a0', pin=elm1.pin['a0']).put()
+        nd.Pin('b0', pin=elm2.pin['b0']).put()
+        nd.Pin('a1', pin=elm3.pin['a0']).put()
+        nd.Pin('b1', pin=elm4.pin['b0']).put()
+        
+    return dc
+
+def shit(X,Y,Lc):
+    # from here I'm building the block
+    with nd.Cell(name='SHIT') as kak:
+        cp1 = coupler(WG_width,Gap,Lc,50,X,Y,bus1,bus2).put()
+    
+        nd.bend(angle=-90,radius=WG_radius,width=WG_width,layer=ld).put(cp1.pin['b0'])
+        nd.sinebend(width=WG_width,distance=400+bus1+Lc+(250-GC_gap+Gap+WG_width)/2,offset=GC_gap-125-(250-GC_gap+Gap+WG_width)/2,layer=ld).put()
+    
+        nd.bend(angle=90,radius=WG_radius,width=WG_width,layer=ld).put(cp1.pin['b1'])
+        nd.sinebend(width=WG_width,distance=400+bus1+Lc+(250-GC_gap+Gap+WG_width)/2,offset=-GC_gap+125+(250-GC_gap+Gap+WG_width)/2,layer=ld).put()
+    
+        nd.bend(angle=-90,radius=50,width=WG_width,layer=ld).put(cp1.pin['a0'])
+        nd.bend(angle=90,radius=(250-GC_gap+Gap+WG_width)/2,width=WG_width,layer=ld).put()
+        nd.strt(length=300,width=WG_width,layer=ld).put()
+    
+        nd.bend(angle=90,radius=50,width=WG_width,layer=ld).put(cp1.pin['a1'])
+        nd.bend(angle=-90,radius=(250-GC_gap+Gap+WG_width)/2,width=WG_width,layer=ld).put()
+        nd.strt(length=300,width=WG_width,layer=ld).put()
+    
+    return kak
+
+
+for i in range(len(Lc)):
+    shit(0,0,Lc[i]).put(0,i*GC_gap*6)
+    nd.strt(length=300,width=WG_width,layer=ld).put(-(350+(250-GC_gap+Gap+WG_width)/2),i*GC_gap*6-2*GC_gap+50+(250-GC_gap+Gap+WG_width)/2)
+    nd.bend(angle=-180,radius=GC_gap/2,width=WG_width,layer=ld).put()
+    nd.strt(length=300,width=WG_width,layer=ld).put()
+    
+    # this part is for the text on the sides
+    f = nd.Font('cousine')
+    f.text('L = '+str(Lc[i])+' um',layer=ld).put(bus1+3*WG_radius+Lc[i],i*GC_gap*6,90)   
+    
+    
+    
+    
+# this part is for the text in the bottom
+nd.strt(length=300,width=WG_width,layer=ld).put(-(350+(250-GC_gap+Gap+WG_width)/2),-142+GC_gap*len(Lc)*6)
+nd.bend(angle=-180,radius=GC_gap/2,width=WG_width,layer=ld).put()
+nd.strt(length=300,width=WG_width,layer=ld).put()
+f.text('Gap = '+str(GC_gap)+' um',layer=ld).put(-(250+(250-GC_gap+Gap+WG_width)/4),-GC_gap*3)   
+
+# do another export in a different layer and file to substract
+nd.export_gds(filename="composite_coupler_sweep.gds")
