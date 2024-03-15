5177d62c7c57ff3ee50b80f3c300a4ef23128a54
rafaelsuna <suna.rafael@gmail.com>
2024-03-15 14:20:24 +0200

    FF

--- Devices/Projects/SBS/grating.py
+++ Devices/Projects/SBS/grating.py
@@ -13,19 +13,22 @@ lib = gdspy.GdsLibrary()
 cell = lib.new_cell('SBS')
 
 # parameters
+priodicity = 1
+FF = 0.8
+grating_period = 10
 center=(0,0)
 radius=10
-inner_radius=9.5
+inner_radius=radius-priodicity*FF
 initial_angle=-0.2*np.pi
 final_angle=0.2*np.pi
 tolerance=0.0001
 
-for i in range(10):
+for i in range(grating_period):
     arc = gdspy.Round(center, radius, inner_radius=inner_radius, initial_angle=initial_angle,
                       final_angle=final_angle, tolerance=tolerance, **LAYER_WG)
     cell.add(arc)
-    radius-=1
-    inner_radius-=1
+    radius-=priodicity
+    inner_radius-=priodicity
 
 
 # Write to GDS file
