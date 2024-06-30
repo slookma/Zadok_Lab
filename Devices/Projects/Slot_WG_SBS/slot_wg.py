fba0df99f8364e8e4eab230cdf7122aa154ba956
slookma <slookma@biu.ac.il>
2024-06-30 14:56:03 +0300

    vds

--- Devices/Projects/Slot_WG_SBS/slot_wg.py
+++ Devices/Projects/Slot_WG_SBS/slot_wg.py
@@ -21,7 +21,7 @@ length = 4400 - 2*(taper_length+small_taper_length)
 clad_width = 4.7
 gaps = [0.04,0.04,0.05,0.05]
 widths = [0.09,0.1,0.11,0.13]
-SOI = 300                   # SOI device layer thickness in nm 
+SOI = 400                   # SOI device layer thickness in nm 
 space = 250                 # spacing between the WGs in Y axis
 L = 0.05
 R = 0.13
@@ -124,11 +124,75 @@ def create_ne_slot_waveguide_with_tapers(gap, left_width, right_width, taper_sta
     return ne_slot
 
 
-create_ne_slot_waveguide_with_tapers(gaps[0], L, R, taper_start_width, taper_length, small_taper_length, layer_number, length).put(0,0)
-nd.strt(length=small_taper_length+taper_length+length,width=clad_width,layer=layer_clad).put()
-nd.bend(angle=180,radius=rad,width=clad_width,layer=layer_clad).put()
-nd.strt(length=small_taper_length+taper_length+length,width=clad_width,layer=layer_clad).put(0,rad*2)
-GC.put(0,0)
+def slot_WG(gap, left_width, right_width, taper_start_width, taper_length, small_taper_length, layer_number, length):
+    """
+    Slot Waveguide.
+    gap                : seperation of the slabs
+    left_width         : width of the left slab
+    right_width         : width of the right slab
+    taper_start_width  : starting width of taper
+    taper_length       : length of taper region
+    small_taper_length : small taper length
+    layer_number       : # of layer for the gds
+    length             : length of the waveguide
+    """
+    with nd.Cell(name='SLOT_WG_BEND') as bent_slot:
+        # Define parameters
+        width_total = left_width + right_width + 2*gap 
+        width_half = width_total/2
+        # start taper from slab WG to ne slot 
+        nd.taper(length=taper_length,width1=taper_start_width,width2=width_total,layer=layer_number).put(0,0) 
+        # tapers splitting for slot
+        nd.taper(length=small_taper_length,width1=width_half,width2=left_width,layer=layer_number).put(taper_length , width_half/2) 
+        nd.taper(length=small_taper_length,width1=width_half,width2=right_width,layer=layer_number).put(taper_length , -width_half/2) 
+        # straight slot waveguide
+        nd.strt(length=length/2,width=left_width,layer=layer_number).put(small_taper_length+taper_length,gap+left_width/2)
+        nd.strt(length=length/2,width=right_width,layer=layer_number).put(small_taper_length+taper_length,-right_width/2)
+        
+        
+        ############################ 
+        ###   doing the bending  ###
+        ############################
+        rad_L = 50+left_width+gap/2
+        rad_R = 50+gap/2+right_width+left_width
+        nd.bend(angle=90,radius=rad_L,width=left_width,layer=layer_number).put(small_taper_length+taper_length+length/2,gap+left_width/2)
+        # nd.bend(angle=90,radius=rad_R,width=right_width,layer=layer_number).put(small_taper_length+taper_length+length/2,-right_width/2)
+        #rad_L+small_taper_length+taper_length+length/2 -left_width/2,(gap+rad_L)
+        nd.bend(angle=-90,radius=rad_L,width=left_width,layer=layer_number).put()
+
+        # nd.bend(angle=-90,radius=50+gap/2+right_width+left_width,width=right_width,layer=layer_number).put(small_taper_length+taper_length+length/2 , -right_width/2)
+        # # backward straight slot waveguide
+        # nd.strt(length=-(length),width=left_width,layer=layer_number).put(small_taper_length+taper_length+length,2*(rad+left_width+gap/2)+gap+left_width/2)
+        # nd.strt(length=-(length),width=right_width,layer=layer_number).put(small_taper_length+taper_length+length,2*(rad+gap/2+right_width+left_width)-right_width/2)
+        
+        # # tapers
+        # nd.taper(length=-small_taper_length,width1=left_width,width2=width_half,layer=layer_number).put(small_taper_length+taper_length,2*(rad+left_width+gap/2)+gap+left_width/2) 
+        # nd.taper(length=-small_taper_length,width1=right_width,width2=width_half,layer=layer_number).put(small_taper_length+taper_length,2*(rad+gap/2+right_width+left_width)-right_width/2)
+
+        # # start taper from ne slot to slabWG
+        # nd.taper(length=-taper_length,width1=width_total,width2=taper_start_width,layer=layer_number).put(taper_length,2*rad+width_total+gap/4) 
+
+
+        ############################
+        
+        # # tpaers for combining slot to slab WG
+        # nd.taper(length=small_taper_length,width1=left_width,width2=width_half,layer=layer_number).put(small_taper_length+taper_length+length,gap+left_width/2) 
+        # nd.taper(length=small_taper_length,width1=right_width,width2=width_half,layer=layer_number).put(small_taper_length+taper_length+length,-right_width/2)
+        # #taper from slot to slab WG
+        # nd.taper(length=taper_length,width1=width_total,width2=taper_start_width,layer=layer_number).put(2*small_taper_length+taper_length+length) 
+
+    return bent_slot
+
+
+
+##########################################################
+##############  This is the main part  ###################
+##########################################################
+slot_WG(gaps[0], L, R, taper_start_width, taper_length, small_taper_length, layer_number, length).put(0,0)
+# nd.strt(length=small_taper_length+taper_length+length,width=clad_width,layer=layer_clad).put()
+# nd.bend(angle=180,radius=rad,width=clad_width,layer=layer_clad).put()
+# nd.strt(length=small_taper_length+taper_length+length,width=clad_width,layer=layer_clad).put(0,rad*2)
+# GC.put(0,0)
 
 
 
@@ -150,56 +214,7 @@ nd.strt(length=970,width=100,layer=layer_clad).put(90,80)
 #         nd.strt(length=80,width=80,layer=layer_clad).put(-270,25+i*space)
 #         nd.strt(length=80,width=80,layer=layer_clad).put(2*small_taper_length+2*taper_length+length+200,25+i*space)
 
-        
-# for j in range(len(gaps)):
-#     # creating the slot
-#     create_slot_waveguide_with_tapers(gaps[j], widths[1], taper_start_width, taper_length, small_taper_length, layer_number, length).put(0,(i+j+2)*space)
-#     nd.strt(length=2*small_taper_length+2*taper_length+length,width=clad_width,layer=layer_clad).put()
-#     f.text('Slot Gap = '+str(int(gaps[j]*1000))+' nm, width = '+str(int(widths[1]*1000)),layer=layer_number).put(100,50+(i+j+2)*space)
-#     nd.strt(length=1000,width=100,layer=layer_clad).put(90,(i+j+2)*space + 80)
-#     f.text(str(i+j+2),layer=layer_number).put(-250,(i+j+2)*space)         # numbering
-#     f.text(str(i+j+2),layer=layer_number).put(2*small_taper_length+2*taper_length+length+220,(i+j+2)*space)         # numbering
-#     if i+j+1 >= 10:
-#         nd.strt(length=100,width=80,layer=layer_clad).put(-270,25+(i+j+2)*space)
-#         nd.strt(length=100,width=80,layer=layer_clad).put(2*small_taper_length+2*taper_length+length+200,25+(i+j+2)*space)
-#     else:
-#         nd.strt(length=80,width=80,layer=layer_clad).put(-270,25+(i+j+2)*space)
-#         nd.strt(length=80,width=80,layer=layer_clad).put(2*small_taper_length+2*taper_length+length+200,25+(i+j+2)*space)
-
-    
-
-# for k in range(len(gaps)):
-#     # creating the slot
-#     create_slot_waveguide_with_tapers(gaps[k], widths[2], taper_start_width, taper_length, small_taper_length, layer_number, length).put(0,(i+j+k+4)*space)
-#     nd.strt(length=2*small_taper_length+2*taper_length+length,width=clad_width,layer=layer_clad).put()
-#     f.text('Slot Gap = '+str(int(gaps[k]*1000))+' nm, width = '+str(int(widths[2]*1000)),layer=layer_number).put(100,50+(i+j+k+4)*space)
-#     nd.strt(length=1000,width=100,layer=layer_clad).put(90,(i+j+k+4)*space + 80)
-#     f.text(str(i+j+k+3),layer=layer_number).put(-250,(i+j+k+4)*space)         # numbering
-#     f.text(str(i+j+k+3),layer=layer_number).put(2*small_taper_length+2*taper_length+length+220,(i+j+k+4)*space)         # numbering
-#     if i+j+k+1 >= 10:
-#         nd.strt(length=100,width=80,layer=layer_clad).put(-270,25+(i+j+k+4)*space)
-#         nd.strt(length=100,width=80,layer=layer_clad).put(2*small_taper_length+2*taper_length+length+200,25+(i+j+k+4)*space)
-#     else:
-#         nd.strt(length=80,width=80,layer=layer_clad).put(-270,25+(i+j+k+4)*space)
-#         nd.strt(length=80,width=80,layer=layer_clad).put(2*small_taper_length+2*taper_length+length+200,25+(i+j+k+4)*space)
-        
-        
-# for m in range(len(gaps)):
-#     # creating the slot
-#     create_slot_waveguide_with_tapers(gaps[m], widths[3], taper_start_width, taper_length, small_taper_length, layer_number, length).put(0,(i+j+k+m+6)*space)
-#     nd.strt(length=2*small_taper_length+2*taper_length+length,width=clad_width,layer=layer_clad).put()
-#     f.text('Slot Gap = '+str(int(gaps[m]*1000))+' nm, width = '+str(int(widths[3]*1000)),layer=layer_number).put(100,50+(i+j+k+m+6)*space)
-#     nd.strt(length=1000,width=100,layer=layer_clad).put(90,(i+j+k+m+6)*space + 80)
-#     f.text(str(i+j+k+m+4),layer=layer_number).put(-250,(i+j+k+m+6)*space)         # numbering
-#     f.text(str(i+j+k+m+4),layer=layer_number).put(2*small_taper_length+2*taper_length+length+220,(i+j+k+m+6)*space)         # numbering
-#     if i+j+k+m+1 >= 10:
-#         nd.strt(length=100,width=80,layer=layer_clad).put(-270,25+(i+j+k+m+6)*space)
-#         nd.strt(length=100,width=80,layer=layer_clad).put(2*small_taper_length+2*taper_length+length+200,25+(i+j+k+m+6)*space)
-#     else:
-#         nd.strt(length=80,width=80,layer=layer_clad).put(-270,25+(i+j+k+m+6)*space)
-#         nd.strt(length=80,width=80,layer=layer_clad).put(2*small_taper_length+2*taper_length+length+200,25+(i+j+k+m+6)*space)
-        
-        
+       
         
 # MS.put(750+length/2,len(gaps)*space) 
 # nd.strt(length=250,width=100,layer=layer_clad).put(630+length/2,len(gaps)*space+25)
